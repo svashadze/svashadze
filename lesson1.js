756b7fb281839e69dbc404e92c5a0832ab0dc543
svashadze <salo.vashadze@gmail.com>
2022-01-30 20:09:56 +0400

    lesson1

---
+++ lesson1.js
@@ -0,0 +1,79 @@
+let user ={
+    firstname:'giorgi',
+    lastname:'smith',
+    age:25,
+    studentstatus:'active',
+} 
+
+console.log(user.studentstatus);
+
+
+
+let number=[200,100,500,800,600,900];
+
+for (let i=0; i<number.length;i++)
+{
+    console.log(number[i]);
+}
+
+
+
+
+let number=[200,100,500,800,600,900];
+
+ i=0
+while ( i<number.length)
+{
+    console.log(number[i]);
+    i++
+}
+
+
+
+let numbers = [5, 6, 10, 25, 3, 7, 4, 100, 28, 36, 89];
+for ( let i=0; i<numbers.length; i++){
+ console.log(numbers[i]);
+}
+
+
+
+let users =  [
+    {username:'test1', status: false},
+    {username:'test2', status: false},
+    {username:'test3', status: true}
+];
+console.log(users[2]);
+
+
+
+let name='giorgi';
+	age= 20;
+    studentstatus = 'active';
+
+if (age > 18 && studentstatus == 'active') {
+    console.log('hello');    
+} 
+else if (name == 'giorgi') 
+  { console.log('hello giorgi');  
+}
+else if (studentstatus == 'active'|| age <25 ) 
+  { console.log('hello world');  
+}
+
+else {
+    console.log('error');
+    
+}
+
+
+    
+let array = [ 'watermelon', 'pear', 10, 45, 50, 'apple', 'ananas' ];
+  
+
+
+
+
+
+    
+
+
