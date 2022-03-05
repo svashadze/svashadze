d2dd5c5a04f6334273631d8e8c73e73fd5c6e1cc
svashadze <salo.vashadze@gmail.com>
2022-03-05 22:14:07 +0400

    lesson3

---
+++ lesson3/lesson3.js
@@ -0,0 +1,7 @@
+let navbar = document.getElementById('navbar');
+let burger = document.getElementById('burger');
+
+
+burger.addEventListener('click', function() {
+    navbar.classList.toggle('navbar');
+})
